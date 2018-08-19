4cd315548712f21e9681a8996f721eeff5d6f9b5
themaighi <miguel.cavazza@gmail.com>
2018-08-19 16:52:12 +0200

    modified progect housemarket and data exploration

---
+++ Housemarket/l - libraries.R
@@ -0,0 +1,18 @@
+list.of.packages <- c(
+  "shiny",
+  "shinydashboard",
+  "data.table",
+  "leaflet",
+  "RSelenium",
+  "httr",
+  "ggmap",
+  "rvest",
+  #"plyr",
+  "XML",
+  "shinycssloaders",
+  "htmltools",
+  "h2o",
+  "curl")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
