0595aa89ae554c12e7664b762d209af0e8f50d59
themaighi <miguel.cavazza@gmail.com>
2018-08-19 22:54:18 +0200

    minor change in the libraries

--- Housemarket/l - libraries.R
+++ Housemarket/l - libraries.R
@@ -16,3 +16,6 @@ list.of.packages <- c(
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+
+lapply(list.of.packages[(list.of.packages %in% installed.packages()[,"Package"])],library, c = TRUE)
+
